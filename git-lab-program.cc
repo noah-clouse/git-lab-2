fdcb328ef913f49e938cc4f171bbf46e1749bc4d
noah-clouse <nc523515@ohio.edu>
2020-05-15 10:06:17 -0400

    Added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Noah Clouse
+ *        Date: Friday, 05/15/2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
