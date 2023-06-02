65355183e9b302881b6c6988f0a647365df63ef9
noah-clouse <nc523515@ohio.edu>
2023-06-02 05:29:16 -0400

    added Hello Git phrase

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Noah Clouse
+ *        Date: 06.02.2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
